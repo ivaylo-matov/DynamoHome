8b084545d9992676825601c5e6caf1316a4b18d6
Ivo Petrov <48355182+ivaylo-matov@users.noreply.github.com>
2026-03-30 07:09:22 +0100

    copy from chloe

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { TemplatesProvider } from './components/TemplatesContext';
 const App = () => {
   const [locale, setLocale] = useState<Locale>("en");
 
-  // Function to get messages based on locale
+  // Function to get messages based on locale new
   const messages = getMessagesForLocale(locale);
 
   useEffect(() => {
